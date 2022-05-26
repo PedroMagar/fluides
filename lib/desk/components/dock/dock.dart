c3df9c028013de2131b696f3c1be6f147e293c2f
Pedro Henrique Martins Garcia <pedro@MacBook-Air-de-Nicolas.local>
2022-05-26 12:22:26 -0300

    Corrigido bordas da sobreposição dos botões

--- lib/desk/components/dock/dock.dart
+++ lib/desk/components/dock/dock.dart
@@ -30,75 +30,78 @@ class Dock extends StatelessWidget {
         color: Colors.white38,
         borderRadius: borderRadiusDefault,
       ),
-      child: Scaffold(
-        backgroundColor: Colors.white.withOpacity(0),
-        body: SafeArea(
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Expanded(
-                flex: 1,
-                child: InkWell(
-                  onTap: () => onStartSelected(),
-                  child: Container(
-                    padding: const EdgeInsets.only(
-                      top: defaultPadding * 0.75,
-                      bottom: defaultPadding * 0.75,
-                      right: defaultPadding * 0.75,
-                      left: defaultPadding * 0.75,
-                    ),
-                    child: SvgPicture.asset(
-                      "assets/icons/start_512x512.svg",
-                      color: Colors.black38,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Container(),
-              ),
-              Expanded(
-                flex: 6,
-                child: InkWell(
-                  onTap: () => onWindowSelected(),
-                  child: Container(
-                    padding: const EdgeInsets.only(
-                      top: defaultPadding * 0.75,
-                      bottom: defaultPadding * 0.75,
-                      right: defaultPadding * 0.75,
-                      left: defaultPadding * 0.75,
-                    ),
-                    child: SvgPicture.asset(
-                      "assets/icons/documents.svg",
-                      color: Colors.black38,
+      child: ClipRRect(
+        borderRadius: borderRadiusDefault,
+        child: Scaffold(
+          backgroundColor: Colors.white.withOpacity(0),
+          body: SafeArea(
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: InkWell(
+                    onTap: () => onStartSelected(),
+                    child: Container(
+                      padding: const EdgeInsets.only(
+                        top: defaultPadding * 0.75,
+                        bottom: defaultPadding * 0.75,
+                        right: defaultPadding * 0.75,
+                        left: defaultPadding * 0.75,
+                      ),
+                      child: SvgPicture.asset(
+                        "assets/icons/start_512x512.svg",
+                        color: Colors.black38,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Container(),
-              ),
-              Expanded(
-                flex: 2,
-                child: InkWell(
-                  onTap: () => onNotificationSelected(),
-                  child: Container(
-                    padding: const EdgeInsets.only(
-                      top: defaultPadding * 0.75,
-                      bottom: defaultPadding * 0.75,
-                      right: defaultPadding * 0.75,
-                      left: defaultPadding * 0.75,
-                    ),
-                    child: SvgPicture.asset(
-                      "assets/icons/menu_notification.svg",
-                      color: Colors.black38,
+                Expanded(
+                  flex: 2,
+                  child: Container(),
+                ),
+                Expanded(
+                  flex: 6,
+                  child: InkWell(
+                    onTap: () => onWindowSelected(),
+                    child: Container(
+                      padding: const EdgeInsets.only(
+                        top: defaultPadding * 0.75,
+                        bottom: defaultPadding * 0.75,
+                        right: defaultPadding * 0.75,
+                        left: defaultPadding * 0.75,
+                      ),
+                      child: SvgPicture.asset(
+                        "assets/icons/documents.svg",
+                        color: Colors.black38,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                Expanded(
+                  flex: 1,
+                  child: Container(),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: InkWell(
+                    onTap: () => onNotificationSelected(),
+                    child: Container(
+                      padding: const EdgeInsets.only(
+                        top: defaultPadding * 0.75,
+                        bottom: defaultPadding * 0.75,
+                        right: defaultPadding * 0.75,
+                        left: defaultPadding * 0.75,
+                      ),
+                      child: SvgPicture.asset(
+                        "assets/icons/menu_notification.svg",
+                        color: Colors.black38,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
